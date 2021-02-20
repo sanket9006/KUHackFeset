b208a120790eb0e505fac05b5f6c1d0fc45e2cdf
unknown <sanket>
2021-02-20 18:56:29 +0530

    message

---
+++ Assets/changescene.cs
@@ -0,0 +1,269 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changescene : MonoBehaviour
+{
+    //Main kindergarden  -  Highersecondary
+     public void historyp()
+    {
+        SceneManager.LoadScene("historyp");
+    }
+
+     public void geographyp()
+    {
+        SceneManager.LoadScene("geographyp");
+    }
+
+    public void geographys()
+    {
+        SceneManager.LoadScene("geographys");
+    }
+
+        public void evsp()
+    {
+        SceneManager.LoadScene("evsp");
+    }
+    
+
+     public void degree()
+    {
+        SceneManager.LoadScene("degree");
+    }
+
+    public void Highersecondary()
+    {
+        SceneManager.LoadScene("Highersecondary");
+    }
+
+     public void secondary()
+    {
+        SceneManager.LoadScene("secondary");
+    }
+
+     public void primary()
+    {
+        SceneManager.LoadScene("primary");
+    }
+
+    public void kindergarden()
+    {
+        SceneManager.LoadScene("kindergarden");
+    }
+
+    public void select()
+    {
+        SceneManager.LoadScene("select");
+    }
+
+
+    public void welcome()
+    {
+        SceneManager.LoadScene("welcome");
+    }
+
+     public void mainmenu()
+    {
+        SceneManager.LoadScene("mainmenu");
+    }
+
+    //Windmill
+    public void windmill()
+    {
+        SceneManager.LoadScene("windmill");
+    }
+
+    //7 Wonders - amphitheatre - eiffeltower - gizamain - tajmahal - statueofliberty - pisa_tower
+    public void amphitheatre()
+    {
+        SceneManager.LoadScene("amphitheatre");
+    }
+
+    public void eiffeltower()
+    {
+        SceneManager.LoadScene("eiffeltower");
+    }
+
+    public void gizamain()
+    {
+        SceneManager.LoadScene("gizamain");
+    }
+
+    public void tajmahal()
+    {
+        SceneManager.LoadScene("tajmahal");
+    }
+
+     public void statueofliberty()
+    {
+        SceneManager.LoadScene("statueofliberty");
+    }
+    
+       public void pisa_tower()
+    {
+        SceneManager.LoadScene("pisa_tower");
+    }
+
+
+
+    //Alpbabets - B- P - R
+
+       public void alphabets()
+    {
+        SceneManager.LoadScene("alphabets");
+    }
+
+         public void one()
+    {
+        SceneManager.LoadScene("one");
+    }
+
+         public void five()
+    {
+        SceneManager.LoadScene("five");
+    }
+
+
+    public void numbers()
+    {
+        SceneManager.LoadScene("numbers");
+    }
+
+    public void ball()
+    {
+        SceneManager.LoadScene("ball");
+    }
+    public void parrot()
+    {
+        SceneManager.LoadScene("parrot");
+    }
+    public void robot()
+    {
+        SceneManager.LoadScene("robot");
+    }
+
+    //Chemistry - Atom
+
+    public void atom()
+    {
+        SceneManager.LoadScene("atom");
+
+    }
+
+    //newtonball interference
+
+    public void interference()
+    {
+        SceneManager.LoadScene("interference");
+
+    }
+
+       public void newtonball()
+    {
+        SceneManager.LoadScene("newtonball");
+
+    }
+
+      public void biology()
+    {
+        SceneManager.LoadScene("biology");
+
+    }
+
+    public void Chemistry()
+    {
+        SceneManager.LoadScene("Chemistry");
+
+    }
+
+     public void motherboard()
+    {
+        SceneManager.LoadScene("motherboard");
+
+    }
+
+    public void physics()
+    {
+        SceneManager.LoadScene("physics");
+
+    }
+
+    public void dna()
+    {
+        SceneManager.LoadScene("dna");
+    }
+
+    public void farming()
+    {
+        SceneManager.LoadScene("farming");
+    }
+
+    //Geography - Hill - Mountain - Waterfall - River
+     public void hill()
+    {
+        SceneManager.LoadScene("hill");
+
+    }
+        public void mountain()
+    {
+        SceneManager.LoadScene("mountain");
+
+    }
+        public void waterfall()
+    {
+        SceneManager.LoadScene("waterfall");
+
+    }
+        public void river()
+    {
+        SceneManager.LoadScene("river");
+
+    }   
+
+    //EVS - Bodypart - Plane - Ship - Car - Stoneage
+
+    public void bodypart()
+    {
+        SceneManager.LoadScene("bodypart");
+
+    }
+    public void plane()
+    {
+        SceneManager.LoadScene("plane");
+
+    }
+
+      public void stoneage()
+    {
+        SceneManager.LoadScene("stoneage");
+
+    }
+        public void ship()
+    {
+        SceneManager.LoadScene("ship");
+
+    }
+
+      public void car()
+    {
+        SceneManager.LoadScene("car");
+
+    }
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
